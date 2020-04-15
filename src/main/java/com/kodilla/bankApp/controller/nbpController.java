12ef3aa14b5c62d99695d2c50c4d31b75b486f54
sebastiankalinkowski <Polska500+>
2020-04-15 14:43:35 +0200

    test2

--- src/main/java/com/kodilla/bankApp/controller/nbpController.java
+++ src/main/java/com/kodilla/bankApp/controller/nbpController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+/*
 @RestController
 @RequestMapping("v1/exchange")
 @CrossOrigin(origins ="*")
@@ -27,5 +27,7 @@ public class nbpController {
 
     }
 }
+*/
+
 
 
